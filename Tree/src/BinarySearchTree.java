49bcffa4f477cd3d787f56b51db2dac8bec3d937
Susanta <maitysusanta822@gmail.com>
2023-12-27 13:27:01 +0530

    Tree

---
+++ Tree/src/BinarySearchTree.java
@@ -0,0 +1,142 @@
+class BinarySearchTree {
+    public class Node {
+        private int val;
+        private Node left;
+        private Node right;
+        private int height;
+
+        public Node(int val) {
+            this.val = val;
+        }
+
+        // getter method
+        public int getValue() {
+            return val;
+        }
+    }
+
+    private Node root;
+
+    public BinarySearchTree() {
+
+    }
+
+    public int height(Node node) { // calculate height of the tree
+
+        if (node == null)
+            return 0;
+        return node.height;
+    }
+
+    public boolean isEmpty() {
+        return root == null;
+    }
+
+    public void insert(int val) {
+        root = insert(root, val);
+    }
+
+    private Node insert(Node node, int val) {
+        if (node == null) {
+            node = new Node(val);
+            return node;
+        }
+        if (val < node.val) {
+            node.left = insert(node.left, val);
+        }
+        if (val > node.val) {
+            node.right = insert(node.right, val);
+        }
+        node.height = 1 + Math.max(height(node.left), height(node.right));
+        return node;
+    }
+
+    public void populate(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            this.insert(nums[i]);
+        }
+    }
+
+    public void populatedSorted(int[] nums) {
+        populatedSorted(nums, 0, nums.length);
+    }
+
+    private void populatedSorted(int[] nums, int start, int end) {
+        if (start >= end) {
+            return;
+        }
+
+        int mid = (start + end) / 2;
+        this.insert(nums[mid]);
+        populatedSorted(nums, start, mid);
+        populatedSorted(nums, mid + 1, end);
+    }
+
+    public boolean balanced() {
+        return balanced(root);
+    }
+
+    private boolean balanced(Node node) {
+        if (node == null)
+            return true;
+        int diff = height(node.left) - height(node.right);
+        if (Math.abs(diff) >= 1)
+            return false;
+        return balanced(node.left) && balanced(node.right);
+    }
+
+    public void printdisplay() {
+        printPretty(this.root, "", true);
+    }
+
+    private void printPretty(Node node, String prefix, boolean isTail) {
+        if (node.right != null) {
+            printPretty(node.right, prefix + (isTail ? "│   " : "    "), false);
+        }
+        System.out.println(prefix + (isTail ? "└── " : "┌── ") + node.getValue());
+        if (node.left != null) {
+            printPretty(node.left, prefix + (isTail ? "    " : "│   "), true);
+        }
+    }
+
+    public void preOrder() {
+        preOrder(root);
+    }
+
+    private void preOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+        System.out.print(node.getValue() + " ");
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
+    public void inOrder() {
+        inOrder(root);
+    }
+
+    private void inOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+        inOrder(node.left);
+        System.out.print(node.getValue() + " ");
+        inOrder(node.right);
+    }
+
+    public void postOrder() {
+        postOrder(root);
+    }
+
+    private void postOrder(Node node) {
+
+        if (node == null) {
+            return;
+        }
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.print(node.getValue() + " ");
+    }
+
+}
---
+++ Tree/src/BinaryTree.java
@@ -0,0 +1,109 @@
+import java.util.Scanner;
+
+public class BinaryTree {
+    private static class Node {
+        Node left;
+        Node right;
+        int value;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    private Node root;
+
+    public void populate(Scanner scanner) {
+        System.out.println("Enter the root nodes");
+
+        int value = scanner.nextInt();
+        root = new Node(value);
+        populate(scanner, root);
+    }
+
+    private void populate(Scanner scanner, Node node) {
+        System.out.println("Do you want to add a left child to " + node.value + "?");
+        boolean left = scanner.nextBoolean();
+        if (left) {
+            System.out.println("Enter the left child of " + node.value);
+            int value = scanner.nextInt();
+            node.left = new Node(value);
+            populate(scanner, node.left);
+        }
+        System.out.println("Do you want to add a right child to " + node.value + "?");
+        boolean right = scanner.nextBoolean();
+        if (right) {
+            System.out.println("Enter the right child of " + node.value);
+            int value = scanner.nextInt();
+            node.right = new Node(value);
+            populate(scanner, node.right);
+        }
+    }
+
+    public void display() {
+        display(this.root, "");
+    }
+
+    private void display(Node node, String prefix) {
+        if (node == null) {
+            return;
+        }
+        System.out.println(prefix + node.value);
+        display(node.left, prefix + "L");
+        display(node.right, prefix + "R");
+    }
+
+    public void printdisplay() {
+        printPretty(this.root, "", true);
+    }
+
+    private void printPretty(Node node, String prefix, boolean isTail) {
+        if (node.right != null) {
+            printPretty(node.right, prefix + (isTail ? "│   " : "    "), false);
+        }
+        System.out.println(prefix + (isTail ? "└── " : "┌── ") + node.value);
+        if (node.left != null) {
+            printPretty(node.left, prefix + (isTail ? "    " : "│   "), true);
+        }
+    }
+
+    public void preOrder() {
+        preOrder(root);
+    }
+
+    private void preOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+        System.out.print("PreOrder Traversal->" + node.value + " ");
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
+    public void inOrder() {
+        inOrder(root);
+    }
+
+    private void inOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+        inOrder(node.left);
+        System.out.print("InOrder Traversal->" + node.value + " ");
+        inOrder(node.right);
+    }
+
+    public void postOrder() {
+        postOrder(root);
+    }
+
+    private void postOrder(Node node) {
+
+        if (node == null) {
+            return;
+        }
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.print("PostOrder Traversal->" + node.value + " ");
+    }
+}
